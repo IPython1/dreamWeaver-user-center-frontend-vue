788c76c4a8b11df70af61f733187592e9b5f2a07
杰杰睡不醒 <93133210+IPython1@users.noreply.github.com>
2024-12-09 23:06:48 +0800

    DW用户中心项目Vue重构
    用户登录 用户注册 用户管理 重新上传

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="homePage">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const msg = "欢迎来到DW用户中心，你将从这里开始学习之旅~";
+</script>
+
+<style scoped>
+#homePage {
+}
+</style>
